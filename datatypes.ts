4bd983ca5a682a09835473ad52ed58ebe3467456
neilmandlik <nrm110604@gmail.com>
2025-08-26 20:04:04 +0530

    Basic Datatypes

---
+++ datatypes.ts
@@ -0,0 +1,17 @@
+let name: string = "Neil";
+
+let newName = name.toUpperCase();
+
+let isValid: boolean = true;
+//in typescript, boolean does not have a predefived value of false
+//it has to be explicitly defined
+
+let age: number = 22;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+
+let sentence: string = `My name is ${name}
+I am ${age} years old.`;
+
+console.log(sentence);
+
